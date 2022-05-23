d08e6e223ff4caa92fc0cbb9473492666d0e0c94
Pedro Salgueiro <pedro.salgueiro@outlook.com>
2022-05-23 15:18:36 +0100

    half working propotype, saving before cleanup and restructure

--- siggen.ino
+++ siggen.ino
@@ -1,76 +1,163 @@
-#include "AD9833.h" // Include the library
 
-#define FNC_PIN 4 // Can be any digital IO pin
+// Include
 
-//--------------- Create an AD9833 object ----------------
-// Note, SCK and MOSI must be connected to CLK and DAT pins on the AD9833 for SPI
-AD9833 gen(9); // Defaults to 25MHz internal reference frequency
+// ads
+#include "AD9833.h" 
+// lcd
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_pinIO.h> // Arduino pin i/o class header
 
-// usually the rotary encoders three pins have the ground pin in the middle
-enum PinAssignments
-{
-    encoderPinA = 2, // right
-    encoderPinB = 3, // left
-    clearButton = 8  // another two pins
-};
+void lcd_setup(void);
+void lcd_loop();
+
+void ads_setup(void);
+void ads_loop(void);
+
+void encoder_setup(void);
+void encoder_loop(void);
+
+const int rs = 8, en = 10, db4 = 4, db5 = 5, db6 = 6, db7 = 7;
+hd44780_pinIO lcd(rs, en, db4, db5, db6, db7);
+const int LCD_COLS = 8;
+const int LCD_ROWS = 2;
+
+const int FNC_PIN = 9;
+AD9833 gen(FNC_PIN);
+
+
+const int encoderPinA = 2; // right
+const int encoderPinB = 3; // left
 
 volatile unsigned int encoderPos = 0; // a counter for the dial
-unsigned int lastReportedPos = 1;     // change management
-static boolean rotating = false;      // debounce management
+volatile long currentFrequency = 50000;
+unsigned int lastReportedPos = 1; // change management
+static boolean rotating = false;  // debounce management
 
 // interrupt service routine vars
 boolean A_set = false;
 boolean B_set = false;
 
+const int F_HZ = 0;
+const int F_KHZ = 1;
+const int F_MHZ = 2;
+
+const int freqUnitPin = A0;
+int freqUnitPinState = LOW;
+int freqUnit = F_HZ;
+
+const int waveShapePin = A1;
+int waveShapeState = LOW;
+
+const int atnPin = A2;
+int atnPinState = LOW;
+
+int buttonState = 0; // current state of the button
+
 void setup()
 {
     pinMode(encoderPinA, INPUT);
     pinMode(encoderPinB, INPUT);
-    pinMode(clearButton, INPUT);
-    // turn on pullup resistors
+
+    pinMode(freqUnitPin, INPUT);
+    pinMode(waveShapePin, INPUT);
+    pinMode(atnPin, INPUT);
+
     digitalWrite(encoderPinA, HIGH);
     digitalWrite(encoderPinB, HIGH);
-    digitalWrite(clearButton, HIGH);
-
-    // encoder pin on interrupt 0 (pin 2)
+    
     attachInterrupt(0, doEncoderA, CHANGE);
-    // encoder pin on interrupt 1 (pin 3)
     attachInterrupt(1, doEncoderB, CHANGE);
 
-    // This MUST be the first command after declaring the AD9833 object
     gen.Begin();
-
-    // Apply a 1 MHz square wave using REG0 (register set 0). There are two register sets,
-    // REG0 and REG1.
-    // Each one can be programmed for:
-    //   Signal type - SINE_WAVE, TRIANGLE_WAVE, SQUARE_WAVE, and HALF_SQUARE_WAVE
-    //   Frequency - 0 to 12.5 MHz
-    //   Phase - 0 to 360 degress (this is only useful if it is 'relative' to some other signal
-    //           such as the phase difference between REG0 and REG1).
-    // In ApplySignal, if Phase is not given, it defaults to 0.
-    gen.ApplySignal(SINE_WAVE, REG0, 12000000);
-
-    gen.EnableOutput(true); // Turn ON the output - it defaults to OFF
-    // There should be a 1 MHz square wave on the PGA output of the AD9833
+    gen.ApplySignal(SINE_WAVE, REG0, currentFrequency);
+    gen.EnableOutput(true);
 
     Serial.begin(9600); // output
+
+    lcd.begin(LCD_COLS, LCD_ROWS);
+    lcd.lineWrap();
+    lcd.print("Signal Gen 1.00");
+
+    Serial.println("Signal Gen 1.00");
 }
 
+int wasLow = 0;
+
 void loop()
-{
-    // To change the signal, you can just call ApplySignal again with a new frequency and/or signal
-    // type.
+{    
 
     rotating = true; // reset the debouncer
 
     if (lastReportedPos != encoderPos)
     {
         lastReportedPos = encoderPos;
+        
+            long scale;
+
+            switch (freqUnit)
+            {
+            case F_HZ:
+                scale = 1;
+                break;
+            case F_KHZ:
+                scale = 1000;
+                break;
+            case F_MHZ:
+                scale = 1000000;
+                break;
+            default:
+                break;
+            }
+
+            currentFrequency += lastReportedPos * scale;
+            lastReportedPos = 0;
+            encoderPos=0;
+
+            char buffer[40];
+            char frequencyText[14];
+            memset(frequencyText, '\0', 14);
+            dtostrf(currentFrequency, 10, 1, frequencyText);
+            sprintf(buffer, "New frequency %s", frequencyText);
+            Serial.println(buffer);
+            gen.SetFrequency(REG0, currentFrequency);
+            lcd.clear();
+            lcd.home();
+            lcd.print(frequencyText);
+        
     }
-    if (digitalRead(clearButton) == LOW)
+
+    buttonState = digitalRead(freqUnitPin);
+    
+    if(buttonState == LOW)
+    {        
+        if(wasLow == 0)
+        {
+            Serial.println("Low");
+        }
+
+        wasLow = 1;
+    }
+    else if(buttonState == HIGH)
     {
-        encoderPos = 0;
+        wasLow = 0;
+        Serial.println("High");
     }
+    
+    if (buttonState != freqUnitPinState)
+    {        
+        if (buttonState == HIGH)
+        {            
+            freqUnit++;
+            if(freqUnit > F_MHZ)
+            {
+                freqUnit = F_HZ;
+            }
+        }
+
+        delay(50);
+    }
+
+    freqUnitPinState = buttonState;
 }
 
 // Interrupt on A changing state
@@ -107,4 +194,46 @@ void doEncoderB()
 
         rotating = false;
     }
+}
+
+// TODO: Move these to modules but lets get these working first
+
+///////////////////////////////////////////////////////////////////////////////
+// LCD
+///////////////////////////////////////////////////////////////////////////////
+
+void lcd_setup(void)
+{
+
+}
+
+void lcd_loop()
+{
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// ADS Chip
+///////////////////////////////////////////////////////////////////////////////
+void ads_setup(void)
+{
+
+}
+
+void ads_loop(void)
+{
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Rotary Encoder
+///////////////////////////////////////////////////////////////////////////////
+void encoder_setup(void)
+{
+
+}
+
+void encoder_loop(void)
+{
+
 }
